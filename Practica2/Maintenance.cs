2f800c4bbd083fd27f1954f4196ffaa745924c4f
DarianR0410 <ram.darian0410@gmail.com>
2025-06-03 20:41:38 -0400

    Meteorological maintenance tracking system.

---
+++ Practica2/Maintenance.cs
@@ -0,0 +1,61 @@
+﻿namespace Practica2;
+
+public class Maintenance
+{
+	public string Path = "C:\\Users\\Darian Ramos\\C#\\Practica2\\LogTracker\\Maintenance.txt";
+
+	private Maintenance()
+	{
+
+	}
+
+	private static Maintenance? _instance;
+
+	public static Maintenance Instance()
+	{
+		if (_instance == null)
+		{
+			_instance = new Maintenance();
+		}
+
+		return _instance;
+	}
+
+	public string CleanSensor(string message)
+	{
+		TextWriter LogWriter = new StreamWriter(Path, true);
+		
+		LogWriter.WriteLine("Date: " + DateTime.Now + " Type: Clear of the sensor. " +  " Note: " + message);
+		LogWriter.Close();
+		
+		return "Mantainance tracked.";
+
+	}
+
+	public string Calibration(string message)
+	{
+		TextWriter logWriter = new StreamWriter(Path, true);
+		logWriter.WriteLine("Date: " + DateTime.Now + " Type: Calibration. " +  " Note: " + message);
+		logWriter.Close();
+		
+		return "Calibration tracked.";
+	}
+
+	public string SensorReplacement(string message)
+	{
+		TextWriter logWriter = new StreamWriter(Path, true);
+		logWriter.WriteLine("Date: " + DateTime.Now + " Type: Sensor replacement. " +  " Note: " + message);
+		logWriter.Close();
+		return "Sensor replacement tracked.";
+	}
+
+	public string GeneralReview(string message)
+	{
+		TextWriter logWriter = new StreamWriter(Path, true);
+		logWriter.WriteLine("Date: " + DateTime.Now + " Type: Sensor replacement. " +  " Note: " + message);
+		logWriter.Close();
+
+		return "General review tracked";
+	}
+	
+}
---
+++ Practica2/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Practica2;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		bool isRunning = true;
+		Maintenance maintenance = Maintenance.Instance();
+		
+
+		while (isRunning)
+		{
+			Console.WriteLine("WELCOME TO THE METHEOROLOGIC STATION MAINTENANCE.");
+			Console.WriteLine("Select one of the following options:");
+			Console.WriteLine("1. Clean sensor.");
+			Console.WriteLine("2. Calibration.");
+			Console.WriteLine("3. Sensor Replacement.");
+			Console.WriteLine("4. General review.");
+			Console.WriteLine("5. Exit.");
+			Console.WriteLine("Enter your message:");
+			string message = Console.ReadLine();
+
+			Console.WriteLine("Select the type of message you would like to send:");
+			int option = Convert.ToInt32(Console.ReadLine());
+
+			switch (option)
+			{
+				case 1: maintenance.CleanSensor(message);
+					break;
+				case 2: maintenance.Calibration(message);
+					break;
+				case 3:
+					maintenance.SensorReplacement(message);
+					break;
+				case 4:
+					maintenance.GeneralReview(message);
+					break;
+				case 5:
+					isRunning = false;
+					break;
+				default: Console.WriteLine("Invalid option.");
+					break;
+			}
+		}
+	}
+}
