56cb2f872c95f1b36ac7a2f837a5f9ab072e2e72
DarianR0410 <ram.darian0410@gmail.com>
2025-06-11 09:02:04 -0400

    UniqueCodes assignment created and tested

---
+++ UniqueCodes/CodeGenerator.cs
@@ -0,0 +1,35 @@
+﻿namespace UniqueCodes;
+
+public class CodeGenerator
+{
+
+	public List<int> generateCode { get; set; }
+	
+	private CodeGenerator()
+	{
+		generateCode = new List<int>();
+	}
+	
+	private static CodeGenerator instance;
+
+	public static CodeGenerator Instance()
+	{
+		if (instance == null)
+		{
+			instance = new CodeGenerator();
+		}
+		
+		return instance;
+	}
+	
+	
+	
+	public int GenerateCode()
+	{
+		int newCode = generateCode.Count + 1;
+		generateCode.Add(newCode);
+		return newCode;
+
+	}
+
+}
---
+++ UniqueCodes/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace UniqueCodes;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		CodeGenerator codeGenerator = CodeGenerator.Instance();
+		bool isRunning = true;
+
+		while (isRunning)
+		{
+			Console.WriteLine("Would you like to generate a unique code? (y/n)");
+			char option = Console.ReadKey().KeyChar;
+
+			if (option == 'y')
+			{
+				Console.WriteLine("Unique code generated: " + codeGenerator.GenerateCode());
+			}
+			else
+			{
+				isRunning = false;
+			}
+		}
+
+	}
+}
